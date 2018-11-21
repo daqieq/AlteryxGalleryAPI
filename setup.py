f3d70ab2e5da3f6ec7263b3a813e51e7f1a76185
daqieq <40532020+daqieq@users.noreply.github.com>
2018-11-20 18:44:34 -0800

    Initial update of setup.py after fork

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="example_pkg",
     version="0.0.1",
-    author="DAVID PRYOR, NICK SIMMONS, AND RITU GOWLIKAR",
+    author="DAVID PRYOR, NICK SIMMONS, RITU GOWLIKAR, and Cameron Willis",
     author_email="david_pryor@quadratic_insights.com",
     description="An API for connecting to an Alteryx Gallery",
     long_description="",
     long_description_content_type="text/markdown",
-    url="https://github.com/Theamazingdp/AlteryxGalleryAPI",
+    url="https://github.com/daqieq/AlteryxGalleryAPI",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
